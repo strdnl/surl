d2ae95419e16772a1c0b613faeccb4f695e20887
strdnl <46500009+strdnl@users.noreply.github.com>
2020-04-04 02:01:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,19 +13,20 @@ for (let i=0;i<storename.length;i++) {
                     
 function makeep(str){
 var ep = digestMessage(str);
+ const ep = digestMessage(str);
  endpoint = "https://www.jsonstore.io/" + ep;
   }
 
-async function digestMessage(message) {
-  const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+function digestMessage(message) {
+  const msgUint8 = new TextEncoder().encode(message);                         
+  const hashBuffer = crypto.subtle.digest('SHA-256', msgUint8);          
+  const hashArray = Array.from(new Uint8Array(hashBuffer));                   
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); 
   return hashHex;
 }
 
-const digestHex = await digestMessage(text);
-console.log(digestHex);
+
+
 
 
 function encrypt(url){
