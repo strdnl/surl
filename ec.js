e2cc8ddecc32e64a5b1d821915c7929cf022d860
strdnl <46500009+strdnl@users.noreply.github.com>
2020-05-26 17:56:52 +0200

    Update ec.js

--- ec.js
+++ ec.js
@@ -1,4 +1,5 @@
-var endpoint  
+var endpoint = "https://www.jsonstore.io/3f372cbb892d082a509f9484d4ea8f7426b30954d1a6cb2255e0a19402bbf419";
+
   
   function send_request(url) {
     this.url = url;
